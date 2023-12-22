cc86ebe92c170ac2c739efac443a81218c7b78ab
Mohamad44p <mohamad44p@yahoo.com>
2023-12-22 16:10:20 +0200

    Edit img

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,13 +13,17 @@ const Main = () => {
       setMovies(response.data.results);
     })
   }, []);
- console.log(movie);
-
+/*console.log(movie);*/
+  const truncate = (str, n) => {
+    if(str?.length > n) {
+      return str.slice(0, n) + '...';
+    }
+    return str;
+  }
   return (
     <div className='w-full h-[550px] text-white'>
-      <div className='w-full h-full'>
+      <div className='w-full h-full' style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})` , backgroundSize: 'cover' , backgroundRepeat: 'no-repeat' , backgroundPosition: 'center'}}>
         <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
-        <img className='w-full h-full object-cover bg-center bg-auto' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
      <div className='absolute w-full top-[20%] p-4 md:p-8'>
         <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
         <div className='my-4'>
@@ -27,7 +31,7 @@ const Main = () => {
           <button className='bg-transparent border px-5 py-2 ml-4 text-white'>Watch Later</button>
         </div>
         <p className='text-gray-400 text-sm'>Released: {movie?.release_date}</p>
-        <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{movie?.overview}</p>
+        <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{truncate(movie?.overview, 150)}</p>
       </div>
       </div>
     </div>
